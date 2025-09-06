6f2d91801c2ffa9a7f5ceab301488d162fc79af4
Sushant Sur <sushant.sur23@gmail.com>
2025-09-06 22:27:30 +0530

    gmail service code updated

---
+++ app/services/gmail_service.py
@@ -0,0 +1,37 @@
+# create_message & save_draft functions
+
+import os, base64, pickle
+from email.mime.text import MIMEText
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.compose']
+
+def create_message(sender, to, subject, message_text):
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
+    return {'message': {'raw': raw}}
+
+def save_draft(sender, to, subject, message_text):
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+    draft_body = create_message(sender, to, subject, message_text)
+    draft = service.users().drafts().create(userId="me", body=draft_body).execute()
+    return draft['id']
